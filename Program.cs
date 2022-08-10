1bde2e53430e82a4a366e2e5c9f8dc7733e2ab0d
Nate S <seilaffn@gmail.com>
2022-08-10 13:17:26 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter Your Favorite Subject");
+            Console.WriteLine("Press 1 for Math. \n Press 2 for Science. \n Press 3 for English. \n" +
+                "Press 4 for Spanish. \n Press 5 for History.");
+            var favSubject = int.Parse(Console.ReadLine());
+            switch (favSubject)
+            {
+                case 1:
+                    Console.WriteLine("Math is your favorite subject");
+                    break;
+                case 2:
+                    Console.WriteLine(" Science is your favorite");
+                    break;
+                case 3:
+                    Console.WriteLine("English is your favorite");
+                    break;
+                case 4:
+                    Console.WriteLine("Spanish is your favorite");
+                    break;
+                case 5:
+                    Console.WriteLine("History is your favorite");
+                    break;
+
+                default:
+                    Console.WriteLine("You dont have a favoite");
+                    break;
+            }
+        }
+    }
+}
